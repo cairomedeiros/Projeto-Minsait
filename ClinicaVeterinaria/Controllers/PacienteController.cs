238a72b1f37b898e115a1acadc07784a75770ba7
cairomedeiros <cairomedeiros13@gmail.com>
2022-10-30 17:09:29 -0300

    controllers paciente e tutor

---
+++ ClinicaVeterinaria/Controllers/PacienteController.cs
@@ -0,0 +1,41 @@
+﻿using ClinicaVeterinaria.Models;
+using ClinicaVeterinaria.Repository;
+using ClinicaVeterinaria.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicaVeterinaria.Controllers {
+    public class PacienteController : Controller {
+        private readonly IPacienteRepository _pacienteRepository;
+
+        [HttpGet]
+        public async Task<ActionResult<List<Paciente>>> RetornarTodosTutores() {
+            List<Paciente> resultado = await _pacienteRepository.RetornarTodosPacientes();
+            return Ok(resultado);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<Paciente>> BuscarPorId(Guid id) {
+            Paciente resultado = await _pacienteRepository.BuscarPorId(id);
+            return Ok(resultado);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Paciente>> Adicionar([FromBody] Paciente paciente) {
+            Paciente resultado = await _pacienteRepository.Adicionar(paciente);
+            return Ok(resultado);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<Paciente>> Editar(Guid id, [FromBody] Paciente paciente) {
+            paciente.Id = id;
+            Paciente resultado = await _pacienteRepository.Editar(id, paciente);
+            return Ok(resultado);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<bool>> DeletarPaciente(Guid id) {
+            bool resultado = await _pacienteRepository.DeletarPaciente(id);
+            return Ok(resultado);
+        }
+    }
+}
---
+++ ClinicaVeterinaria/Controllers/TutorController.cs
@@ -0,0 +1,50 @@
+﻿using ClinicaVeterinaria.Models;
+using ClinicaVeterinaria.Repository.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicaVeterinaria.Controllers {
+
+    [Route("api/[controller")]
+    [ApiController]
+    public class TutorController : ControllerBase {
+
+        private readonly ITutorRepository _tutorRepository;
+
+        public TutorController(ITutorRepository tutorRepository) { 
+            _tutorRepository = tutorRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Tutor>>> RetornarTodosTutores() { 
+            List<Tutor> resultado = await _tutorRepository.RetornarTodosTutores();
+            return Ok(resultado);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<Tutor>> BuscarPorId(Guid id) {
+            Tutor resultado = await _tutorRepository.BuscarPorId(id);
+            return Ok(resultado);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Tutor>> Adicionar([FromBody] Tutor tutor) {
+            Tutor resultado = await _tutorRepository.Adicionar(tutor);
+            return Ok(resultado);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<Tutor>> Editar(Guid id, [FromBody] Tutor tutor) {
+            tutor.Id = id;
+            Tutor resultado = await _tutorRepository.Editar(id, tutor);
+            return Ok(resultado);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<bool>> DeletarTutor(Guid id) {
+            bool resultado = await _tutorRepository.DeletarTutor(id);
+            return Ok(resultado);
+        }
+
+    }
+}

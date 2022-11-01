8e996c1adeb931d37fb0131fbfed02adb9923caf
cairomedeiros <cairomedeiros13@gmail.com>
2022-11-01 09:54:58 -0300

    adicionando novos tutores e pacientes ao seed

--- ClinicaVeterinaria/Data/SeedingService.cs
+++ ClinicaVeterinaria/Data/SeedingService.cs
@@ -14,11 +14,20 @@ namespace ClinicaVeterinaria.Data {
                 return;
             }
 
-            Tutor tutor1 = new(new Guid(), "Cairo", "12312332124", "cabedelo", "99999999", "12/22/2000");
-            Paciente paciente1 = new(new Guid(), "Nymeria", "Cachorro", "Pastor alemão", tutor1, 12, 20, "preto", "Dr. Júlio");
+            Tutor tutor1 = new(new Guid(), "Cairo", "12312332124", "Cabedelo", "99999999", "05/07/2000");
+            Tutor tutor2 = new(new Guid(), "Bob Esponja", "12312332124", "Oceano", "99999999", "12/22/2000");
+            Tutor tutor3 = new(new Guid(), "Naruto", "12312332124", "Konoha", "99999999", "16/10/1990");
+            Tutor tutor4 = new(new Guid(), "Maria Rita", "12312332124", "João Pessoa", "99999999", "02/05/2002");
 
-            _context.Tutores.AddRange(tutor1);
-            _context.Pacientes.AddRange(paciente1);
+            Paciente paciente1 = new(new Guid(), "Nymeria", "Cachorro", "Pastor alemão", 4, 20, "Preta", "Dr. Júlio", tutor1);
+            Paciente paciente2 = new(new Guid(), "Mel", "Cachorro", "Shitzu", 7, 20, "Marrom", "Dr. Júlio", tutor4);
+            Paciente paciente3 = new(new Guid(), "Nemo", "Peixe", "Peixe palhaço", 20, 1, "Laranja com listras", "Dr. Júlio", tutor1);
+            Paciente paciente4 = new(new Guid(), "Gary", "Caracol", "Caracol do mar", 16, 10, "Rosa", "Dr. Júlio", tutor2);
+            Paciente paciente5 = new(new Guid(), "Gamakichi", "Sapo", "Sapo ninja", 100, 30, "Laranja", "Dr. Jiraya", tutor3);
+            Paciente paciente6 = new(new Guid(), "Kurama", "Raposa", "Raposa de nove caldas", 90, 102, "preto", "Dr. Jiraya", tutor3);
+
+            _context.Tutores.AddRange(tutor1, tutor2, tutor3, tutor4);
+            _context.Pacientes.AddRange(paciente1, paciente2, paciente3, paciente4, paciente5, paciente6);
 
             _context.SaveChanges();
 
--- ClinicaVeterinaria/Models/Paciente.cs
+++ ClinicaVeterinaria/Models/Paciente.cs
@@ -5,22 +5,22 @@
         public string Nome { get; set; }
         public string Especie { get; set; }
         public string Raca { get; set; }
-        public Tutor Tutor { get; set; }
         public int Idade { get; set; }
         public float Peso { get; set; }
         public string Cor { get; set; }
         public string MedicoResponsavel { get; set; }
+        public Tutor Tutor { get; set; }
 
-        public Paciente(Guid id, string nome, string especie, string raca, Tutor tutor, int idade, float peso, string cor, string medicoResponsavel) {
+        public Paciente(Guid id, string nome, string especie, string raca, int idade, float peso, string cor, string medicoResponsavel, Tutor tutor) {
             Id = id;
             Nome = nome;
             Especie = especie;
             Raca = raca;
-            Tutor = tutor;
             Idade = idade;
             Peso = peso;
             Cor = cor;
             MedicoResponsavel = medicoResponsavel;
+            Tutor = tutor;
         }
 
         public Paciente() {

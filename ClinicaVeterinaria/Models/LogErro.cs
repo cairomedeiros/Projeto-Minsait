99d881c0e96f214444dcfee79fa8aff19426951e
cairomedeiros <cairomedeiros13@gmail.com>
2022-11-09 08:46:50 -0300

    adicionando model LogErro, para tratamento de erros

---
+++ ClinicaVeterinaria/Models/LogErro.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicaVeterinaria.Models {
+    public class LogErro {
+
+        [Key]
+        public Guid Id { get; set; }
+        [Required, MaxLength(500)]
+        public string StackTrace { get; set; }
+        [Required, MaxLength(200)]
+        public string Mensagem { get; set; }
+        [MaxLength(500)]
+        public string? InnerException { get; set; }
+        [Required]
+        public DateTime DataHoraRegistro { get; set; }
+    }
+}
